4fe91b5a506f1931cdd510026e809dcd2f150651
ThermoDev <michael.pegios@hotmail.com>
2020-08-06 02:31:29 +1000

    Implementing Custom History Object

---
+++ client/src/history.js
@@ -0,0 +1,6 @@
+// Custom Browser History object
+// 'history' is available automatically from react-router-dom
+import { createBrowserHistory } from 'history'; 
+
+// Export our newly created browser history
+export default createBrowserHistory();
