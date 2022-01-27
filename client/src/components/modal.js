b8a0201bcb56f8dd1693967b1b870a5f96acc2bd
Michael Pegios <michael.pegios@hotmail.com>
2022-01-27 20:11:47 +1100

    Reusable Modal
    - Added props to the Modal. improving reusability
    - Created props in StreamDelete

--- client/src/components/modal.js
+++ client/src/components/modal.js
@@ -1,22 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import history from '../history';
 
 const modal = (props) => {
   return ReactDOM.createPortal(
     <div
-      onClick={() => history.push('/')}
+      onClick={props.onDismiss}
       className="ui dimmer modals visible active"
     >
-      <div onClick={(e) => e.stopPropagation()} className="ui standard modal visible active">
-        <div className="header">Delete Stream</div>
-        <div className="content">
-          Are you sure you want to delete the stream?
-        </div>
-        <div className="actions">
-          <button className="ui primary button">Delete</button>
-          <button className="ui button">Cancel</button>
-        </div>
+      <div
+        onClick={(e) => e.stopPropagation()}
+        className="ui standard modal visible active"
+      >
+        <div className="header">{props.title}</div> v
+        <div className="content">{props.content}</div>
+        <div className="actions">{props.actions}</div>
       </div>
     </div>,
     document.querySelector('#modal')
